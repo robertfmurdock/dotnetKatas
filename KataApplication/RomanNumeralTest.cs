7a63c4a997b268981a595939ab1c8f9109fbb101
Rob Murdock <rmurdock@pillartechnology.com>
2017-05-10 14:00:02 -0400

    Rob: Stripping kata down to basics!

--- KataApplication/RomanNumeralTest.cs
+++ KataApplication/RomanNumeralTest.cs
@@ -9,72 +9,11 @@ namespace KataApplication
     {
 
         [Test]
-        public void repeatersCanRepeat_I()
+        public void ShouldReturn1ForI()
         {
-            checkNumeral("I", 1);
-            checkNumeral("II", 2);
-            checkNumeral("III", 3);
-        }
-
-        [Test]
-        public void repeatersCanRepeat_X()
-        {
-            checkNumeral("X", 10);
-            checkNumeral("XX", 20);
-            checkNumeral("XXX", 30);
-        }
-        [Test]
-        public void repeatersCanRepeat_C()
-        {
-            checkNumeral("C", 100);
-            checkNumeral("CC", 200);
-            checkNumeral("CCC", 300);
-        }
-        [Test]
-        public void repeatersCanRepeat_M()
-        {
-            checkNumeral("M", 1000);
-            checkNumeral("MM", 2000);
-            checkNumeral("MMM", 3000);
-        }
-
-        [Test]
-        public void shouldSubtractRepeaterSymbolFromHalfwaySymbol_4ForIV()
-        {
-            checkNumeral("IV", 4);
-        }
-
-        [Test]
-        public void shouldSubtractRepeaterSymbolFromRepeaterSymbol_9ForIX()
-        {
-            checkNumeral("IX", 9);
-        }
-
-        [Test]
-        public void shouldSubtractRepeaterSymbolFromHalfwaySymbol_40ForXL()
-        {
-            checkNumeral("XL", 40);
-        }
-
-        [Test]
-        public void shouldReturn5ForV()
-        {
-            checkNumeral("V", 5);
-        }
-        [Test]
-        public void shouldReturn50ForL()
-        {
-            checkNumeral("L", 50);
-        }
-        [Test]
-        public void shouldReturn500ForD()
-        {
-            checkNumeral("D", 500);
-        }
-
-        private void checkNumeral(string stringToConvert, int expectedResult)
-        {
-            Assert.AreEqual(expectedResult, RomanNumeralConverter.convert(stringToConvert));
+            var actualResult = RomanNumeralConverter.Convert("I");
+            var expectedResult = 1;
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
     }
